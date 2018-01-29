f3b9ebfc9fe6098510f51ffd066f48c9a4b08f2d
SanaNasar <sanasehereen.cse@gmail.com>
2018-01-28 18:25:52 -0800

    Adding twitter auth

---
+++ authentication/TwitterAuth/app.js
@@ -0,0 +1,26 @@
+var provider = new firebase.auth.TwitterAuthProvider();
+
+function twitterSignin() {
+   firebase.auth().signInWithPopup(provider)
+    
+  .then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+    
+      console.log(token);
+      console.log(user);
+   }).catch(function(error) {
+      console.log(error.code);
+      console.log(error.message);
+   });
+}
+
+function twitterSignout() {
+   firebase.auth().signOut()
+   
+   .then(function() {
+      console.log('Signout successful!');
+   }, function(error) {
+      console.log('Signout failed!');
+   });
+}
